2557ffce1a4336da7c4cfc416e2ce2ad664e3ee1
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-05-30 17:32:38 -0300

    commit inicial

---
+++ GeraItemAleatoriamente/Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] itens;
+    [SerializeField] private GameObject referencia;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(
+                itens[Random.Range(0, itens.Length)],
+                referencia.transform
+                );
+        }
+    }
+}
